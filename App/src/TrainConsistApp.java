675f9df2547fdda059d4106b76a07f3139657ede
KaranP315 <karan2006.p@gmail.com>
2026-04-09 15:10:40 +0530

    Karan P Added UC10 Stream Reduce Total Capacity

--- App/src/TrainConsistApp.java
+++ App/src/TrainConsistApp.java
@@ -1,10 +1,9 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Bogie {
     String name;
     int capacity;
-    String type; // Passenger / Goods
+    String type;
 
     public Bogie(String name, int capacity, String type) {
         this.name = name;
@@ -16,6 +15,7 @@ class Bogie {
         return name + " (" + capacity + ")";
     }
 }
+
 
 public class TrainConsistApp {
 
@@ -29,21 +29,13 @@ public class TrainConsistApp {
         bogies.add(new Bogie("Sleeper", 72, "Passenger"));
         bogies.add(new Bogie("AC Chair", 60, "Passenger"));
         bogies.add(new Bogie("First Class", 24, "Passenger"));
-        bogies.add(new Bogie("Cargo Box", 100, "Goods"));
-        bogies.add(new Bogie("Oil Tank", 120, "Goods"));
 
-        // Step 2: Group using Stream
-        Map<String, List<Bogie>> grouped = bogies.stream()
-                .collect(Collectors.groupingBy(b -> b.type));
+        // Step 2: Stream + map + reduce
+        int totalCapacity = bogies.stream()
+                .map(b -> b.capacity)         // extract capacity
+                .reduce(0, Integer::sum);     // sum all values
 
-        // Step 3: Display grouped data
-        System.out.println("\nGrouped Bogies:");
-
-        for (Map.Entry<String, List<Bogie>> entry : grouped.entrySet()) {
-            System.out.println("\nType: " + entry.getKey());
-            for (Bogie b : entry.getValue()) {
-                System.out.println("  " + b);
-            }
-        }
+        // Step 3: Display result
+        System.out.println("\nTotal Seating Capacity: " + totalCapacity);
     }
 }
